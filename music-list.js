05d755fac800733ea2a4276267de21371368438b
Deepanjali Kushwaha <deepanjalikushwaha1211@gmail.com>
2025-06-23 12:47:29 +0530

    Create music-list.js

---
+++ music-list.js
@@ -0,0 +1,24 @@
+
+
+let allMusic = [
+  {
+    name: "Appadi Podu",
+    artist: "Sriram",
+    
+    src: "songs_music-2"
+  },
+  {
+    name: "Kokkarakko",
+    artist: "Udit Narayan , Sujatha Mohan",
+  
+    src: "songs_music-3"
+  },
+  {
+    name: "Sha La La",
+    artist: "Sunidhi Chauhan",
+  
+    src: "songs_music-4"
+  },
+ 
+  
+];
